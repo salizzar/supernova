72b10766d91f73aee0b42541b96974b9462199be
Marcelo Correia Pinheiro <salizzar@gmail.com>
2022-09-03 20:08:33 -0300

    feat(azure): wip

--- terraform/website/azure.tf
+++ terraform/website/azure.tf
@@ -83,6 +83,7 @@ resource "azurerm_storage_account" "website" {
   location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
+  min_tls_version          = "TLS1_2"
 
   blob_properties {
     versioning_enabled = true
