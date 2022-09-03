82621bdf8c33fcba8b3bde2382ba4e2980179349
Marcelo Correia Pinheiro <salizzar@gmail.com>
2022-09-03 20:00:04 -0300

    feat(azure): wip

---
+++ terraform/website/aws.tf
@@ -0,0 +1,213 @@
+provider "aws" {
+  profile = local.aws_profile
+  region  = local.aws_region
+
+  default_tags {
+    tags = {
+      Project     = local.domain
+      Environment = terraform.workspace
+    }
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = local.domain
+}
+
+module "acm" {
+  depends_on = [
+    aws_route53_zone.main
+  ]
+
+  providers = {
+    aws.src = aws
+    aws.dns = aws
+  }
+
+  source = "git::ssh://git@github.com/salizzar/terraform-modules.git//aws/acm-certificate?ref=v1.0.8"
+
+  aws_acm_certificate = {
+    domain_name               = local.domain
+    subject_alternative_names = local.subject_alternative_names
+    tags                      = {}
+  }
+
+  aws_route53_zone = {
+    name = aws_route53_zone.main.name
+  }
+}
+
+#tfsec:ignore:aws-cloudfront-enable-logging
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  depends_on = [
+    module.acm,
+    aws_cloudfront_origin_access_identity.oai,
+    module.s3-website,
+    aws_wafv2_web_acl.waf
+  ]
+
+  origin {
+    domain_name = module.s3-website.bucket_domain_name
+    origin_id   = module.s3-website.id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "Marcelo Pinheiro"
+  default_root_object = "index.html"
+  web_acl_id          = aws_wafv2_web_acl.waf.arn
+
+  # TODO: enable this paraphernalia in the future
+  #logging_config {
+  #  include_cookies = true
+  #  bucket          = module.s3-website-cf-logs.bucket_domain_name
+  #  prefix          = "websites/marcelopinheiro.co/cloudfront-logs"
+  #}
+
+  aliases = concat([local.domain], local.subject_alternative_names)
+
+  custom_error_response {
+    error_caching_min_ttl = 10
+    error_code            = 404
+    response_code         = 404
+    response_page_path    = "/404.html"
+  }
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = module.s3-website.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "https-only"
+    min_ttl                = 0
+    default_ttl            = local.one_day_in_seconds
+    max_ttl                = local.one_day_in_seconds * local.days_per_month
+  }
+
+  # Cache behavior with precedence 0
+  ordered_cache_behavior {
+    path_pattern     = "*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = module.s3-website.id
+
+    forwarded_values {
+      query_string = false
+      headers      = ["Origin"]
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = local.one_day_in_seconds * local.days_per_month
+    max_ttl                = local.one_year_in_seconds
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  # Cache behavior with precedence 1
+  ordered_cache_behavior {
+    path_pattern     = "/*"
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = module.s3-website.id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = local.one_day_in_seconds * local.days_per_month
+    max_ttl                = local.one_year_in_seconds
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  price_class = "PriceClass_200"
+
+  viewer_certificate {
+    cloudfront_default_certificate = false
+    acm_certificate_arn            = module.acm.certificate-id
+    minimum_protocol_version       = "TLSv1.2_2021"
+    ssl_support_method             = "sni-only"
+  }
+}
+
+resource "aws_wafv2_web_acl" "waf" {
+  name  = "${local.sanitized_domain}_waf"
+  scope = "CLOUDFRONT"
+
+  default_action {
+    allow {}
+  }
+
+  visibility_config {
+    cloudwatch_metrics_enabled = true
+    metric_name                = "${local.sanitized_domain}_waf"
+    sampled_requests_enabled   = true
+  }
+}
+
+resource "aws_route53_record" "cloudfront" {
+  zone_id = aws_route53_zone.main.id
+  name    = local.domain
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_route53_record" "subject_alternative_names" {
+  for_each = toset(local.subject_alternative_names)
+
+  zone_id = aws_route53_zone.main.id
+  name    = each.key
+  type    = "CNAME"
+  records = [aws_cloudfront_distribution.s3_distribution.domain_name]
+  ttl     = 3600
+}
+
+#data "external" "aws-sync" {
+#  depends_on = [
+#    module.s3-website
+#  ]
+#
+#  program = [
+#    "/bin/bash", "bin/aws-website-sync"
+#  ]
+#
+#  query = {
+#    folder                     = "../../website/"
+#    bucket                     = module.s3-website.id
+#    profile                    = local.aws_profile
+#    cloudfront_distribution_id = aws_cloudfront_distribution.s3_distribution.id
+#  }
+#}
+
---
+++ terraform/website/azure.tf
@@ -0,0 +1,162 @@
+provider "azurerm" {
+  features {}
+}
+
+variable "AZURE_TENANT_ID" {
+  type = string
+}
+
+variable "AZURE_SUBSCRIPTION_ID" {
+  type = string
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = local.domain
+  location = "East US 2"
+}
+
+resource "azurerm_subscription" "subscription" {
+  subscription_name = "Azure subscription"
+  subscription_id   = var.AZURE_SUBSCRIPTION_ID
+}
+
+data "azuread_client_config" "current" {}
+
+resource "azuread_application" "app" {
+  display_name = local.domain
+  owners       = [data.azuread_client_config.current.object_id]
+}
+
+resource "azuread_service_principal" "marcelopinheiro" {
+  application_id               = azuread_application.app.application_id
+  app_role_assignment_required = true
+  owners                       = [data.azuread_client_config.current.object_id]
+
+  #app_roles = [
+  #  data.azurerm_role_definition.blob-data-owner.id
+  #]
+
+  #oauth2_permission_scopes = [
+  #  {
+  #    id = "/subscriptions/${azurerm_subscription.subscription.alias}/resourceGroups/${azurerm_resource_group.rg.name}"
+  #  }
+  #]
+}
+
+data "azurerm_subscription" "primary" {
+}
+
+data "azurerm_client_config" "client" {
+}
+
+#data "azurerm_role_definition" "blob-data-owner" {
+#  role_definition_id = "b7e6dc6d-f1e8-4753-8033-0f276bb0955b" # "b24988ac-6180-42a0-ab88-20f7382dd24c"
+#  scope              = data.azurerm_subscription.primary.id
+#}
+
+#resource "azurerm_role_definition" "blob-data-owner" {
+#  role_definition_id = "b7e6dc6d-f1e8-4753-8033-0f276bb0955b" # "00000000-0000-0000-0000-000000000000"
+#  name               = "marcelopinheiro-co_blob-data-owner"
+#  scope              = data.azurerm_subscription.primary.id
+#
+#  permissions {
+#    actions     = ["Microsoft.Resources/subscriptions/resourceGroups/read"]
+#    not_actions = []
+#  }
+#
+#  assignable_scopes = [
+#    data.azurerm_subscription.primary.id,
+#  ]
+#}
+
+
+#resource "azurerm_role_assignment" "assignment" {
+#  name               = azurerm_role_definition.blob-data-owner.role_definition_id
+#  scope              = data.azurerm_subscription.primary.id
+#  role_definition_id = azurerm_role_definition.blob-data-owner.role_definition_id
+#  principal_id       = data.azurerm_client_config.client.object_id
+#}
+
+resource "azurerm_storage_account" "website" {
+  name                     = local.domain_without_dot
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+
+  blob_properties {
+    versioning_enabled = true
+  }
+
+  # custom_domain {
+  #   name = local.domain
+  # }
+
+  static_website {
+    index_document     = "index.html"
+    error_404_document = "404.html"
+  }
+}
+
+resource "azurerm_storage_container" "website" {
+  name                  = "website"
+  storage_account_name  = azurerm_storage_account.website.name
+  container_access_type = "container"
+}
+
+#data "external" "azure-sync" {
+#  depends_on = [
+#    azurerm_storage_container.website
+#  ]
+#
+#  program = [
+#    "/bin/bash", "bin/azure-website-sync"
+#  ]
+#
+#  query = {
+#    folder                = "../../website/"
+#    tenant_id             = var.AZURE_TENANT_ID
+#    storage_container_url = "https://${local.domain_without_dot}.blob.core.windows.net/${azurerm_storage_container.website.name}"
+#  }
+#}
+
+#data "external" "compress-website" {
+#  program = [
+#    "/bin/bash", "bin/compress"
+#  ]
+#
+#  query = {
+#    zip_file_path = "../../website.zip"
+#
+#    zip_folder_files = join(" ", [
+#      "../../website/index.html",
+#      "../../website/404.html",
+#      "../../website/styes",
+#      "../../website/images"
+#    ])
+#  }
+#}
+#
+#resource "azurerm_storage_blob" "website" {
+#  name                   = local.domain_without_dot
+#  storage_account_name   = azurerm_storage_account.marcelopinheiro.name
+#  storage_container_name = azurerm_storage_container.marcelopinheiro.name
+#  type                   = "Block"
+#  source                 = data.external.compress-website.result["zip_file_path"]
+#}
+
+output "azurerm_resource_group" {
+  value = azurerm_resource_group.rg.location
+}
+
+output "azuread_application_id" {
+  value = azuread_application.app.id
+}
+
+output "azuread_service_principal_application_tenant_id" {
+  value = azuread_service_principal.marcelopinheiro.application_tenant_id
+}
+
+#output "azurerm_storage_blob_url" {
+#  value = azurerm_storage_blob.website.url
+#}
