17196c2b484e9d99aa0e5ace6614a3c25138ab02
Marcelo Correia Pinheiro <salizzar@gmail.com>
2022-11-08 23:14:34 -0300

    feat(azure): wip

--- terraform/website/azure.tf
+++ terraform/website/azure.tf
@@ -1,10 +1,10 @@
 provider "azurerm" {
-  features {}
-
   subscription_id = var.AZURE_SUBSCRIPTION_ID
   client_id       = var.AZURE_CLIENT_ID
   client_secret   = var.AZURE_CLIENT_SECRET
   tenant_id       = var.AZURE_TENANT_ID
+
+  features {}
 }
 
 resource "azurerm_resource_group" "rg" {
